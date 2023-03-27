367ff1f38e622dc95c75dec57e2658c192159f59
adrianojec <adrianojeric.28@gmail.com>
2023-03-27 22:05:25 +0800

    authentication

---
+++ API/Controllers/Users/AuthController.cs
@@ -0,0 +1,80 @@
+using System.Security.Claims;
+using API.Controllers.Users.InputModels;
+using API.Controllers.Users.ViewModels;
+using API.Services;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers.Users
+{
+   [ApiController]
+   [Route("api/[controller]")]
+
+   public class AuthController : ControllerBase
+   {
+      private readonly UserManager<User> _userManager;
+      private readonly TokenService _tokenService;
+      public AuthController(UserManager<User> userManager, TokenService tokenService)
+      {
+         _tokenService = tokenService;
+         _userManager = userManager;
+      }
+
+      [AllowAnonymous]
+      [HttpPost("login")]
+      public async Task<ActionResult<UserViewModel>> login([FromBody] LoginUserInputModel input)
+      {
+         var user = await _userManager.FindByEmailAsync(input.Email);
+
+         if (user == null) return Unauthorized();
+
+         var result = await _userManager.CheckPasswordAsync(user, input.Password);
+
+         if (!result) return Unauthorized();
+
+         var token = _tokenService.CreateToken(user);
+
+         return new UserViewModel(user.UserName, token);
+      }
+
+      [AllowAnonymous]
+      [HttpPost("register")]
+      public async Task<ActionResult<UserViewModel>> register([FromBody] RegisterUserInputModel input)
+      {
+         if (await _userManager.Users.AnyAsync(user => user.UserName == input.UserName))
+         {
+            return BadRequest("Username is already taken.");
+         }
+
+         if (await _userManager.Users.AnyAsync(user => user.Email == input.Email))
+         {
+            return BadRequest("Email is already taken.");
+         }
+
+         var user = input.ToUserEntity();
+
+         var result = await _userManager.CreateAsync(user, input.Password);
+
+         if (!result.Succeeded) return BadRequest(result.Errors);
+
+         var token = _tokenService.CreateToken(user);
+
+         System.Console.WriteLine(token);
+
+         return new UserViewModel(user.UserName, token);
+      }
+
+      [HttpGet]
+      public async Task<ActionResult<UserViewModel>> GetCurrentUser()
+      {
+         var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+         var token = _tokenService.CreateToken(user);
+
+         return new UserViewModel(user.UserName, token);
+      }
+   }
+}
---
+++ API/Program.cs
@@ -0,0 +1,37 @@
+using API.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Database Connection
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentityService(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+   app.UseSwagger();
+   app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
